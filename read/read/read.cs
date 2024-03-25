38a3ef1433994fd51f6c097349a44bb7d7922d40
MS Mthethwa <mbekezelimthethwa@gmail.com>
2024-03-25 23:06:01 +0200

    Learn How to Read and Write Text Files in C# | Beginner's Guide

---
+++ read/read/read.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+class read
+{
+    static void Main()
+    {
+        try
+        {
+            //Create a Stream reader object
+            StreamReader sr = new StreamReader("C:/Users/CC/OneDrive/Desktop/writeNread.txt");
+
+            //Create a list to store line from text document
+            var lineList = new List<string>();
+
+            //read the whole text document
+            String line;
+
+            while ((line = sr.ReadLine()) != null){
+                lineList.Add(line);
+            }
+
+            sr.Close();
+
+            //Convert text lines from list to array
+            String[] textFile = lineList.ToArray();
+
+            //Display all text stored on the array
+            foreach (string text in textFile ) { 
+                Console.WriteLine(text);
+            }
+
+        }
+        catch(Exception e) { 
+        
+            Console.WriteLine("Exception error: \n" + e);
+        
+        }
+        Console.ReadLine();
+    }
+}
---
+++ write/write/write.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class write
+{
+    static void Main()
+    {
+        try {
+
+            using (StreamWriter sw = new StreamWriter ("C:/Users/CC/OneDrive/Desktop/writeNread.txt"))
+            {
+                sw.WriteLine("You are tasked with creating a C# project to handle text file operations.");
+            }
+
+            Console.WriteLine("Successful updated text document");
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception error: " + e);
+        }
+        Console.Read();
+    }
+
+}
